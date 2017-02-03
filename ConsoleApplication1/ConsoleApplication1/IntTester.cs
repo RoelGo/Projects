b71a864bd975b33f4fe3adbcd57ecb7a9e179a10
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-03 10:47:43 +0100

    My first console application

---
+++ ConsoleApplication1/ConsoleApplication1/IntTester.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class IntTester
+    {
+        public int TestInt { get; set; }
+
+        public IntTester(int testInt)
+        {
+            this.TestInt = testInt;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Wtf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Wtf
+    {
+        static void Main(string[] args)
+        {
+            IntTester intTester = new IntTester(5);
+            Console.WriteLine(intTester.TestInt);
+            intTester.TestInt = 10;
+            Console.WriteLine(intTester.TestInt);
+            Console.ReadLine();
+        }
+    }
+}
