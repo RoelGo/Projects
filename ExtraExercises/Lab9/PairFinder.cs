423a87584380c43379bada96bf5853c353ea9cf8
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-06 15:57:18 +0100

    YAYA

---
+++ ExtraExercises/Lab9/PairFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab9
+{
+    internal class PairFinder
+    {
+        public static List<Tuple<int, int>> FindPairs(int[] input, int sum)
+        {
+//            int[,] output = new int[input.GetLength(0),2];
+            List<Tuple<int,int>> output = new List<Tuple<int, int>>();
+            for (int index = 0; index < input.GetLength(0); index++)
+            {
+                for (int nextIndex = index; nextIndex < input.GetLength(0); nextIndex++)
+                {
+                    if (input[index] + input[nextIndex] == sum)
+                    {
+                        output.Add(new Tuple<int, int>(input[index], input[nextIndex]));
+//                        output[index, 0] = input[index];
+//                        output[index, 1] = input[nextIndex];
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ ExtraExercises/Lab9/PairFinderTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab9
+{
+    [TestClass]
+    public class PairFinderTest
+    {
+        [TestMethod]
+        public void FindPairsInEmptyArrayShouldReturnEmpty()
+        {
+            int[] testArray = new int[1];
+
+            var expected = false;
+            var actual = PairFinder.FindPairs(testArray,1).Any();
+
+            Assert.AreEqual(expected,actual);
+
+        }
+
+        [TestMethod]
+        public void FindPairsInArrayShouldRetrunTheRightPairsShort()
+        {
+            int[] testArray = new int[] {5, 4, 6};
+
+            var expected = new List<Tuple<int,int>>()
+            {
+                new Tuple<int, int>(5, 4)
+            };
+
+            var actual = PairFinder.FindPairs(testArray, 9);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+                [TestMethod]
+                public void FindPairsInArrayShouldRetrunTheRightPairsLong()
+                {
+                    int[] testArray = new int[] {5, 4, 6, 21, 3, 4, 8, 1, 23, 85, 54};
+        
+                    var expected = new List<Tuple<int, int>>()
+                    {
+                       new Tuple<int, int>(5, 4),
+                       new Tuple<int, int>(5, 4),
+                       new Tuple<int, int>(6, 3),
+                       new Tuple<int, int>(8, 1)
+            
+                    };
+                    var actual = PairFinder.FindPairs(testArray, 9);
+        
+        
+                    CollectionAssert.AreEqual(expected, actual);
+                }
+    }
+}
