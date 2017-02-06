c9e185ce7acf1d6f4632fa30befc82ac1bc37431
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-06 12:00:25 +0100

    Ugh

---
+++ ExtraExercises/MultiDimentionalComparison/ArrayComparator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultiDimentionalComparison
+{
+    public class ArrayComparator
+    {
+        internal static string[,] Compare(int[,] array1, int[,] array2)
+        {
+            string[,] output =
+                new string[Math.Min(array1.GetLength(0), array2.GetLength(0)),
+                    Math.Min(array1.GetLength(1), array2.GetLength(1))];
+            for (int row = 0; row < output.GetLength(0); row++)
+            {
+                for (int collumn = 0; collumn < output.GetLength(1); collumn++)
+                {
+                    if (array1[row, collumn] != array2[row, collumn])
+                    {
+                        output[row, collumn] = array1[row, collumn] + " -- " + array2[row, collumn];
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ ExtraExercises/MultiDimentionalComparison/MultiDimentionalComparisonTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static MultiDimentionalComparison.ArrayComparator;
+
+namespace MultiDimentionalComparison
+{
+    [TestClass]
+    public class MultiDimentionalComparisonTest
+    {
+        [TestMethod]
+        public void NoDifferenceShouldReturnEmptyArray()
+        {
+            int[,] testArray =
+            {
+                {4, 2, 5},
+                {5, 2, 4},
+                {4, 5, 2}
+            };
+
+            string expected = new string[3, 3].ToString();
+            string actual = Compare(testArray, testArray).ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DifferenceShouldReturnStringyArray()
+        {
+            int[,] testArray =
+            {
+                {4, 2, 5},
+                {5, 2, 4},
+                {4, 5, 2}
+            };
+            int[,] testArray2 =
+            {
+                {4, 2, 5},
+                {5, 3, 4},
+                {4, 5, 2}
+            };
+
+            string expected = new string[,]
+            {
+                {null, null, null},
+                {null, "2 -- 3", null},
+                {null, null, null}
+            }.ToString();
+
+            string actual = Compare(testArray, testArray2).ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
